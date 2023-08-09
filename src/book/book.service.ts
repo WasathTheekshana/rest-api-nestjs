2aa4c329be6d11e4801bb23c32d45bbc9a42175c
WasathatLayoutIndex <wasath.theekshana@layoutindex.lk>
2023-08-09 11:07:08 +0530

    feat: pagination & search feature

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -3,6 +3,7 @@ import { Book } from './schemas/book.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import * as mongoose from 'mongoose';
 
+import { Query } from 'express-serve-static-core';
 @Injectable()
 export class BookService {
   constructor(
@@ -11,8 +12,23 @@ export class BookService {
   ) {}
 
   //Read all books
-  async findAll(): Promise<Book[]> {
-    const books = await this.bookModel.find();
+  async findAll(query: Query): Promise<Book[]> {
+    
+    const resPerPage = 2 // results per page
+    const currentPage = Number(query.page) || 1 
+    const skip = resPerPage * (currentPage - 1) 
+
+
+    const keyword = query.keyword
+      ? {
+          title: {
+            $regex: query.keyword,
+            $options: 'i',
+          },
+        }
+      : {};
+
+    const books = await this.bookModel.find({ ...keyword }).limit(resPerPage).skip(skip);
     return books;
   }
 
